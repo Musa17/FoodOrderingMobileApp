a8d5e211524e0f5ccd9086d713a9d68f6a500a69
Musa17 <amusul00@fesb.hr>
2021-10-17 21:26:57 +0200

    Adding CategoryDataService class

---
+++ FoodOrderingApp/FoodOrderingApp/Services/CategoryDataService.cs
@@ -0,0 +1,33 @@
+﻿using Firebase.Database;
+using FoodOrderingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingApp.Services
+{
+    public class CategoryDataService
+    {
+        FirebaseClient client;
+
+        public CategoryDataService()
+        {
+            client = new FirebaseClient("https://foodorderingapp-cd3d8-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            var categories = (await client.Child("Categories").OnceAsync<Category>()).Select(c => new Category
+            {
+                CategoryID = c.Object.CategoryID,
+                CategoryName = c.Object.CategoryName,
+                CategoryPoster = c.Object.CategoryPoster,
+                ImageUrl = c.Object.ImageUrl
+            }).ToList();
+
+            return categories;
+        }
+    }
+}
