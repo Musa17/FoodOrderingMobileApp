ac934f8e525ca4e58cc12da8182a1705a055f495
Musa17 <amusul00@fesb.hr>
2021-10-17 16:55:41 +0200

    Finishing Login form

--- FoodOrderingApp/FoodOrderingApp/App.xaml.cs
+++ FoodOrderingApp/FoodOrderingApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodOrderingApp.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace FoodOrderingApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new LoginView();
         }
 
         protected override void OnStart()
