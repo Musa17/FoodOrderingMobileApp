84c692b6c81ac0b68d96a9414f2b3a6dcf93da00
Musa17 <amusul00@fesb.hr>
2021-10-21 11:42:43 +0200

    Adding connection to SQLite

---
+++ FoodOrderingApp/FoodOrderingApp.Android/SQLite_Android.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FoodOrderingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(FoodOrderingApp.Droid.SQLite_Android))]
+namespace FoodOrderingApp.Droid
+{
+    public class SQLite_Android : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFileName = "MyDatabase.db3";
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFileName);
+            var cn = new SQLiteConnection(path);
+            return cn;
+        }
+    }
+}
---
+++ FoodOrderingApp/FoodOrderingApp/Model/ISQLite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodOrderingApp.Model
+{
+    public interface ISQLite
+    {
+
+    }
+}
